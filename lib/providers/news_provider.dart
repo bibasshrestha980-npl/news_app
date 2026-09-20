7e5adc406a2a07bbfe702d5b6b6bd6628fe2a72c
Bibas Shrestha <bibasshrestha980@gmail.com>
2026-09-20 20:00:09 +0545

    Fetch trending news using http.get and log response

--- lib/providers/news_provider.dart
+++ lib/providers/news_provider.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:developer' as logger;
 
 class NewsProvider extends ChangeNotifier {
   Future getTrendingNews() async {
     String url =
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=c0a8fe7d946c41a79725fe5c4add934d";
+    final response = await http.get(Uri.parse(url));
+    logger.log(response.body);
   }
 }
-
