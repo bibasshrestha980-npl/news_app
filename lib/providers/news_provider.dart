f8375681efa38b1e306a902d411ce6b8d46970fc
Bibas Shrestha <bibasshrestha980@gmail.com>
2026-09-20 19:55:05 +0545

    Add provider package and news_provider.dart with API key

---
+++ lib/providers/news_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NewsProvider extends ChangeNotifier {
+  Future getTrendingNews() async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=c0a8fe7d946c41a79725fe5c4add934d";
+  }
+}
