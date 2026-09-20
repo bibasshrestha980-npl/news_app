41e201c67ffd643e0ae9de759996e6192566a782
Bibas Shrestha <bibasshrestha980@gmail.com>
2026-09-20 19:57:19 +0545

    Add http package and import to news_provider.dart

--- lib/providers/news_provider.dart
+++ lib/providers/news_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class NewsProvider extends ChangeNotifier {
   Future getTrendingNews() async {
@@ -6,3 +7,4 @@ class NewsProvider extends ChangeNotifier {
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=c0a8fe7d946c41a79725fe5c4add934d";
   }
 }
+
